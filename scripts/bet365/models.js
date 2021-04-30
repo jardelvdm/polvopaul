28fc1e73b9b2f2f638e79677e6469390e4f79643
jardelvdm <jardelvdm@gmail.com>
2021-05-01 00:12:17 +0100

    add arquivo de models pra padronizar relação de dados no script e no DB

---
+++ scripts/bet365/models.js
@@ -0,0 +1,26 @@
+module.exports = {
+    modelClube: clube => ({
+        abbr: clube.abbr,
+        founded: clube.founded,
+        iscountry: clube.iscountry,
+        mediumname: clube.mediumname,
+        name: clube.name,
+        nickname: clube.nickname,
+        sex: clube.sex,
+        suffix: clube.suffix,
+        bet365: clube._id,
+        country: pais
+    }),
+    modelJogador: jogador => ({
+        abbr: clube.abbr,
+        founded: clube.founded,
+        iscountry: clube.iscountry,
+        mediumname: clube.mediumname,
+        name: clube.name,
+        nickname: clube.nickname,
+        sex: clube.sex,
+        suffix: clube.suffix,
+        bet365: clube._id,
+        country: pais
+    })
+}
