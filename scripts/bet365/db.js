e9959fb0be39a007aacd8f4562d8f14c0b499075
jardelvdm <jardelvdm@gmail.com>
2021-05-01 01:20:10 +0100

    implementa alguns metodos sql

--- scripts/bet365/db.js
+++ scripts/bet365/db.js
@@ -1,18 +1,40 @@
 // manipular o banco de dados
+let connection;
 module.exports = {
-    connect: () => {
+    connect: () => new Promise(resolve => {
         var mysql      = require('mysql');
-        var connection = mysql.createConnection({
+        connection = mysql.createConnection({
             host     : 'localhost',
             user     : 'root',
             password : '',
             database : 'polvopaul'
         });
 
-        let r = connection.connect();
+        connection.connect();
 
-        return r;
-    }
+        resolve(connection);
+    }),
+    campeonatos: () => new Promise((resolve,reject) => {
+        connection.query("SELECT * FROM campeonatos WHERE bet365_season IS NOT NULL", function (error, campeonatos, fields) {
+            if(error) reject(error)
+
+            resolve(campeonatos);
+        })  
+    }),
+    campeonatosPais: (paisID) => new Promise((resolve,reject) => {
+        connection.query(`SELECT * FROM paises INNER JOIN campeonatos ON paises.id = campeonatos.pais WHERE paises.id = ${paisID}`, function (error, campeonatos, fields) {
+            if(error) reject(error)
+
+            resolve(campeonatos);
+        })  
+    }),
+    campeonato: (campeonatoID) => new Promise((resolve,reject) => {
+        connection.query(`SELECT * FROM campeonatos WHERE campeonatos.id = ${campeonatoID} LIMIT 1`, function (error, campeonatos, fields) {
+            if(error) reject(error)
+
+            resolve(campeonatos);
+        })  
+    })
     // registrar os times da liga
     // registrar os jogadores dos times
     // registrar as partidas de todos os times
