9bee8eaa099b41436346f3e6056c89fcb38de82e
jardelvdm <jardelvdm@gmail.com>
2021-05-06 00:12:11 +0100

    Inicia uma UI basica pra inputar os dados fora do prompt

---
+++ UI/server.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+var path = require('path')
+var express = require('express');
+var app = express();
+var http = require('http')
+var https = require('https');
+const fs = require('fs');
+var bodyParser = require('body-parser');
+
+const api = require('../scripts/bet365/api'),
+      db = require('../scripts/bet365/db');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json())
+
+http.createServer(app).listen(7777);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, './index.html'))
+});
