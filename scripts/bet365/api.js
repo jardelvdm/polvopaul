2b727914d8fc8bed3a01bc703fbc5e965effa0dd
jardelvdm <jardelvdm@gmail.com>
2021-05-05 23:54:46 +0100

    [scripts bet365] adiciona metodos de trazer estatisticas de times e jogadores

--- scripts/bet365/api.js
+++ scripts/bet365/api.js
@@ -6,6 +6,7 @@ const API = {
             overUnder: (idTemporada) => new Promise((resolve,reject) => fetch(`https://stats.fn.sportradar.com/bet365/br/Europe:London/gismo/stats_season_overunder/${idTemporada}`).then(res => res.json()).then(res => resolve(res.doc[0].data)).catch(err => reject(err))),
             fixtures: (idTemporada) => new Promise((resolve,reject) => fetch(`https://stats.fn.sportradar.com/bet365/br/Europe:London/gismo/stats_season_fixtures2/${idTemporada}/1`).then(res => res.json()).then(res => resolve(res.doc[0].data)).catch(err => reject(err))),
             uniqueteamstats: (idTemporada) => new Promise((resolve,reject) => fetch(`https://stats.fn.sportradar.com/bet365/br/Europe:Berlin/gismo/stats_season_uniqueteamstats/${idTemporada}`).then(res => res.json()).then(res => resolve(res.doc[0].data)).catch(err => reject(err))),
+            teamplayer_facts: (idTemporada,idTime) => new Promise((resolve,reject) => fetch(`https://stats.fn.sportradar.com/bet365/br/Europe:London/gismo/stats_teamplayer_facts/${idTime}/${idTemporada}`).then(res => res.json()).then(res => resolve(res.doc[0].data)).catch(err => reject(err))),
         },
         match: {
             stats_match_get: (idPartida) => new Promise((resolve,reject) => fetch(`https://stats.fn.sportradar.com/bet365/en/Europe:Berlin/gismo/stats_match_lineup/${idPartida}`).then(res => res.json()).then(res => resolve(res.doc[0].data)).catch(err => reject(err))),
@@ -50,7 +51,8 @@ const API = {
     }),
     timeEsperado: (idTime,idTemporada) => new Promise((resolve,reject) => fetch(`https://stats.fn.sportradar.com/bet365/br/Europe:Berlin/gismo/stats_team_usuallineup/${idTime}/${idTemporada}`).then(res => res.json()).then(res => resolve(res.doc[0].data)).catch(err => reject(err))),
     overunderTimes: (idTemporada) =>  new Promise((resolve,reject) => API.stats.season.overUnder(idTemporada).then(data => resolve(Object.values(data.stats))).catch(err => reject(err))),
-    teamstats: (idTemporada) =>  new Promise((resolve,reject) => API.stats.season.uniqueteamstats(idTemporada).then(data => resolve(Object.values(data.stats.uniqueteams))).catch(err => reject(err)))
+    teamstats: (idTemporada) =>  new Promise((resolve,reject) => API.stats.season.uniqueteamstats(idTemporada).then(data => resolve(Object.values(data.stats.uniqueteams))).catch(err => reject(err))),
+    jogadoresStats: (idTemporada,idTime) =>  new Promise((resolve,reject) => API.stats.season.teamplayer_facts(idTemporada,idTime).then(data => resolve(Object.values(data))).catch(err => reject(err))),
 }
 
 module.exports = API;
