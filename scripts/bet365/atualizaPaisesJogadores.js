cc7307b04febb2be6168ff58732c68928750482d
jardelvdm <jardelvdm@gmail.com>
2021-05-06 00:01:10 +0100

    [scripts bet365] script pra alterar as nacionalidades de jogadores (ao inves do nome do pais, adiciona o ID do pais)

---
+++ scripts/bet365/atualizaPaisesJogadores.js
@@ -0,0 +1,15 @@
+const models = require('./models'),
+      API = require('./api'),
+      db = require('./db');
+
+db.connect().then(() => {
+    db.freestyle(`SELECT * FROM paises`).then(paises => {
+        console.log(paises);
+
+        paises.forEach(pais => {
+            db.freestyle(`UPDATE jogadores SET jogadores.nationality = (SELECT id FROM paises WHERE paises.nome = '${pais.nome}' LIMIT 1)
+            WHERE jogadores.nationality = '${pais.nome}'`)
+        });
+    })
+
+})
